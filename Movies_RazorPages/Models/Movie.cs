9716d33f2da7ade5dcb82bbfe8d502e56561aece
juliedosher <juliegdosher@lewisu.edu>
2023-06-30 14:42:24 -0500

    completed requirements

--- Movies_RazorPages/Models/Movie.cs
+++ Movies_RazorPages/Models/Movie.cs
@@ -6,20 +6,30 @@ namespace Movies_RazorPages.Models
     {
         public int ID { get; set; }
 
+        [Required]
         public string Title { get; set; }
 
+        [Required]
         public string Director { get; set; }
 
+        [Required]
         public string Synopsis { get; set; }
 
         [Display(Name = "Runtime (minutes)")]
+        [Required]
+        [StringLength(3, MinimumLength = 1)]
         public int RuntimeMin { get; set; }
 
+
+        [StringLength(4, MinimumLength = 4)]
         [Display(Name = "Release Year")]
+        [Required]
         public int ReleaseYear { get; set; }
 
+        [Required]
         public string Language { get; set; }
 
+        [Required]
         public string Genre { get; set; }
     }
 }
---
+++ Movies_RazorPages/Pages/About.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Movies_RazorPages.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
